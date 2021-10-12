34e925e65c21d7028f678b668f3e87d15a54038c
Le Tu Oanh <ltoanh.laxy@gmail.com>
2021-10-12 15:34:25 +0700

    Revert "api/axios installed"

--- src/containers/Homepage/BestSeller/BestSeller.jsx
+++ src/containers/Homepage/BestSeller/BestSeller.jsx
@@ -1,15 +1,8 @@
-import React, { useEffect } from "react";
+import React from 'react';
 //=================== style========================
-import "./bestseller.css";
-//=================== api =========================
-import { baseURL } from "../../../api-config/axiosConfig";
-import axios from "axios";
+import './bestseller.css';
 
 function BestSeller() {
-  useEffect(() => {
-    axios.get(baseURL + "/best-foods").then((res) => console.log(res.data));
-  }, []);
-
   return (
     <section className="container best-seller">
       <p className="small-title">Best seller</p>
