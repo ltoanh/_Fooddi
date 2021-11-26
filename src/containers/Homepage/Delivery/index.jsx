195d8dae96263479b7f4f30248b18381c768d83f
whiwf <leetusoanh@gmail.com>
2021-11-26 10:51:59 +0700

    update ui deliver

--- src/containers/Homepage/Delivery/index.jsx
+++ src/containers/Homepage/Delivery/index.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import fooddiApi from "api-config/fooddiApi";
+import React, { useEffect, useState } from "react";
 import "./delivery.css";
+import DeliveryItem from "./DeliveryItem";
 
 function Delivery() {
+  const [listDeliveryEmployee, setListDeliveryEmployee] = useState([]);
+
+  // fetch api employee
+  useEffect(() => {
+    const fetchEmployee = async () => {
+      try {
+        let response = await fooddiApi.getEmployeeList();
+        let deliveryList = response.filter(
+          (employee) => employee.position === "delivery"
+        );
+        console.log(response);
+        console.log(deliveryList);
+        setListDeliveryEmployee(deliveryList);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchEmployee();
+  }, []);
+
   return (
     <section id="delivery">
       <div className="container">
@@ -11,7 +33,19 @@ function Delivery() {
           <br />
           On Right Time & Place
         </h2>
-        
+        <div className="delivery-list">
+          {listDeliveryEmployee &&
+            listDeliveryEmployee.map((deliveryMan, id) => (
+              <DeliveryItem
+                key={id}
+                name={deliveryMan.name}
+                imgSrc={deliveryMan.img_src}
+                phoneNumber={deliveryMan.phone_number}
+                satisfied={deliveryMan.satisfied}
+                unsatisfied={deliveryMan.unsatisfied}
+              />
+            ))}
+        </div>
       </div>
     </section>
   );
